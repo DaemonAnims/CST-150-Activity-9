6df5ba53527c27a07e482432c42f3b581e160363
DaemonAnims <daemonanimations3d@gmail.com>
2022-07-28 19:35:52 -0400

    Add project files.

---
+++ Activity 9/MethodStubs.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity_9
+{
+    public class MethodStubs
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Method Stubs Have been completed!");
+        }
+
+        public void showSum(int x, int y) { }
+
+        public double averageFive(double a, double b, double c, double d, double e) { return 0.0; }
+
+        public int sumRandomTwo() { return 0; }
+
+        public bool isSumDivisibleByThree(int x, int y, int z) { return false; }
+
+        public void showSmallerString(string s, string t) { }
+
+        public double getLargestDouble(double[] doubles) { return 0.0; }
+
+        public int[] get50RandomIntegers() { return new int[50]; }
+
+        public bool isSameBoolValue(bool bool1, bool bool2) { return false; }
+
+        public double productOfIntAndDouble(int x, double y) { return 0.0; }
+
+        public double averageOfTwoDimensionalArray(int[][] ints) { return 0.0; }
+
+    }
+}
